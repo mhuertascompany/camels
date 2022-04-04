b61bc89df52c3c8ba194cf2e5db27dcd268aa1c4
mhuertascompany <marc.huertas@obspm.fr>
2022-04-04 15:04:37 +0100

    scripts for datasets

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
- from setuptools import setup
+from setuptools import find_packages
+from setuptools import setup
 
- setup(
-   name='Camels',
-   version='0.1.0',
-   author='TFG Cecilia',
-   author_email='mhuertas@iac.es',
-   url='https://github.com/mhuertascompany/camels',
-   packages=find_packages(),
-   install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
-)
+setup(name='camels',
+      description='camels package',
+      author='AstroInfo',
+      packages=find_packages(),
+      install_requires=['astropy', 'tensorflow_datasets']
+      )
