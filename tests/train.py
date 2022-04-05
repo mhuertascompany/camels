d9511da33255ecfd89a79217694b94498acc5021
Cecilia-adc <alu0101254548@ull.edu.es>
2022-04-05 17:55:10 +0100

    adding s to dataset

--- tests/train.py
+++ tests/train.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 import tensorflow_datasets as tfds
 
-# load dataset
+# load datasets
 
 data_dir  = "/net/diva/scratch-ssd1/mhuertas/users.flatironinstitute.org/~fvillaescusa/priv/DEPnzxoWlaTQ6CjrXqsm0vYi8L7Jy/CMD/2D_maps/data"
 #dset = tfds.load('maps', split='train', data_dir=data_dir)
