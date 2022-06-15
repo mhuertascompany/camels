e8a67c155a8f5742b23095f20ea229cc5d184855
Cecilia-adc <alu0101254548@ull.edu.es>
2022-06-15 19:07:01 +0100

    cambiar el class

--- datasets/TNG.py
+++ datasets/TNG.py
@@ -16,17 +16,42 @@ _CITATION = ""
 _URL = "https://github.com/mhuertascompany/camels"
 
 
-def _split_generators(self, dl):
-  """Returns generators according to split"""
-  return {tfds.Split.TRAIN: self._generate_examples(str(dl.manual_dir))}
+class TNG(tfds.core.GeneratorBasedBuilder):
+  VERSION = tfds.core.Version("1.0.0")
+  RELEASE_NOTES = {'1.0.0': 'Initial release.', }
+  MANUAL_DOWNLOAD_INSTRUCTIONS = "Nothing to download. Dataset was generated at first call."
 
+  def _info(self) -> tfds.core.DatasetInfo:
+    N_TIMESTEPS = 100
+    return tfds.core.DatasetInfo(
+      builder=self,
+      description=_DESCRIPTION,
+      homepage=_URL,
+      citation=_CITATION,
+      # Two features: image with 3 channels (stellar light, velocity map, velocity dispersion map)
+      #  and redshift value of last major merger
+      features=tfds.features.FeaturesDict({
+        'Mgas': tfds.features.Tensor(shape=(256, 256, 1), dtype=tf.float32),
+        'Mstar': tfds.features.Tensor(shape=(256, 256, 1), dtype=tf.float32),
+        'omega_m': tf.float32,
+        'sigma_8': tf.float32,
+        "A_sn1": tf.float32,
+        "A_agn1": tf.float32,
+        "A_sn2": tf.float32,
+        "A_agn2": tf.float32
+      }),
+      supervised_keys=('noiseless_griz', 'last_major_merger'),
+    )
 
-def load_maps(maptype):
+  def _split_generators(self, dl):
+    """Returns generators according to split"""
+     return {tfds.Split.TRAIN: self._generate_examples(str(dl.manual_dir))}
 
-  fmaps = root_path+'/Maps_'+maptype+'_IllustrisTNG_LH_z=0.00.npy'
-  maps  = np.load(fmaps)
-  fparams = root_path +'/params_IllustrisTNG.txt'
-  params  = np.loadtxt(fparams)
-  return maps,params
+  def load_maps(maptype):
+    fmaps = root_path+'/Maps_'+maptype+'_IllustrisTNG_LH_z=0.00.npy'
+    maps  = np.load(fmaps)
+    fparams = root_path +'/params_IllustrisTNG.txt'
+    params  = np.loadtxt(fparams)
+    return maps,params
 
 
